afed1f2c16dbf96e7627086d830556a89adb2237
MillicentMalinga <90625689+MillicentMalinga@users.noreply.github.com>
2024-02-11 21:13:39 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+     
+      'sm': '100px',
+      // => @media (min-width: 100px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        'logo': ['Protest Guerrilla', 'sans-serif'],
+        'body-plex': ['IBM Plex Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+});

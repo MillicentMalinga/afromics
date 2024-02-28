4ee47cf8f67702565f8a1619b63fb971f01b637b
Millicent Malinga <90625689+MillicentMalinga@users.noreply.github.com>
2024-02-29 00:37:49 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -14,7 +14,7 @@ function Dashboard() {
     <div className='bg-white '>
 <DashBar usrName="Millicent" />
 <form action="">
-<div className="flex lg:flex-row sm:flex-col w-3/5 self-center border-[1px] border-blue-gray-800 mx-auto my-20 shadow-lg rounded-lg">
+<div className="flex lg:flex-row sm:flex-col lg:w-3/5 sm:w-4/5 self-center border-[1px] border-blue-gray-800 mx-auto my-20 shadow-lg rounded-lg">
   <div className="lg:w-1/2  sm:w-full flex sm:border-b-[1px] sm:border-b-blue-gray-500 flex-row px-4 gap-2 self-center  ">
   <FontAwesomeIcon icon={faMagnifyingGlass}  className='text-blue-gray-600 self-center'/>
   <input type='text' className='w-full font-body-plex py-4 lg:border-r-[1px] border-blue-gray-700' placeholder='Search for projects, opportunities, datasets'/> </div>

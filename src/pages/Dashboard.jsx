45676e91b24b64e710ea75b97b6ece40f1e21c3e
Millicent Malinga <90625689+MillicentMalinga@users.noreply.github.com>
2024-02-29 00:33:52 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,10 +15,10 @@ function Dashboard() {
 <DashBar usrName="Millicent" />
 <form action="">
 <div className="flex lg:flex-row sm:flex-col w-3/5 self-center border-[1px] border-blue-gray-800 mx-auto my-20 shadow-lg rounded-lg">
-  <div className="w-1/2 flex flex-row px-4 gap-2 self-center  ">
+  <div className="lg:w-1/2  sm:w-full flex sm:border-b-[1px] sm:border-b-blue-gray-500 flex-row px-4 gap-2 self-center  ">
   <FontAwesomeIcon icon={faMagnifyingGlass}  className='text-blue-gray-600 self-center'/>
-  <input type='text' className='w-full font-body-plex py-4 border-r-[1px] border-blue-gray-700' placeholder='Search for projects, opportunities, datasets'/> </div>
-  <div className="w-1/2 flex gap-2 flex-row px-4 self-center  ">
+  <input type='text' className='w-full font-body-plex py-4 lg:border-r-[1px] border-blue-gray-700' placeholder='Search for projects, opportunities, datasets'/> </div>
+  <div className="lg:w-1/2 sm:w-full sm:border-b-[1px] sm:border-b-blue-gray-500 flex gap-2 flex-row px-4 self-center  ">
   <FontAwesomeIcon icon={faLocationDot}  className='text-blue-gray-600 self-center'/>
   <input type='text' className=' w-full font-body-plex py-4' placeholder='City, or Country'/>
   <input type="submit" value="Search" className='bg-blue-gray-700 px-6 py-0 h-8 self-center rounded-md font-body-plex text-white' />
